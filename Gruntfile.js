18de839e9bc54301b6204066017c965347161537
zmanring <zmanring@gmail.com>
2013-11-08 16:04:03 -0500

    Code cleanup, adding grunt jasmine,

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,73 +1,100 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+  // Load all tasks
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
   // Project configuration.
   grunt.initConfig({
-   pkg: grunt.file.readJSON('package.json'),
-   cssc: {
-     build: {
-       options: {
-         consolidateViaDeclarations: true
-       },
-       files: {
-         'build/css/main.css': 'build/css/main.css'
-       }
-     }
-   },
-   cssmin: {
-     build: {
-       src: 'build/css/main.css',
-       dest: 'build/css/main.css'
-     }
-   },
-   htmlhint: {
-     build: {
-       options: {
-         'tag-pair': true,
-         'tagname-lowercase': true,
-         'attr-lowercase': true,
-         'attr-value-double-quotes': true,
-         'doctype-first': true,
-         'spec-char-escape': true,
-         'id-unique': true,
-         'head-script-disabled': true,
-         'style-disabled': true
-       },
-       src: ['index.html']
-     }
-   },
-   uglify: {
-     build: {
-       files: {
-         'build/js/main.min.js': ['assets/js/jquery-2.0.3.min.js', 'assets/js/main.js']
-       }
-     }
-   },
-   sass: {
-     build: {
-       files: {
-         'build/css/main.css': 'assets/scss/main.scss'
-       }
-     }
-   },
-   watch: {
-    css: {
-      files: ['assets/scss/**/*.scss'],
-      tasks: ['buildcss']
+    pkg: grunt.file.readJSON('package.json'),
+    cssc: {
+      build: {
+        options: {
+          consolidateViaDeclarations: true
+        },
+        files: {
+          'build/css/main.css': 'build/css/main.css'
+        }
+      }
     },
-    html: {
-      files: ['index.html'],
-      tasks: ['htmlhint']
+    cssmin: {
+      build: {
+        src: 'build/css/main.css',
+        dest: 'build/css/main.css'
+      }
     },
-    js: {
-      files: ['assets/js/main.js'],
-      tasks: ['uglify']
+    copy: {
+      indexHtml: {
+        src: 'index.html',
+        dest: 'build/index.html',
+      },
+    },
+    htmlhint: {
+      build: {
+        options: {
+          'attr-lowercase': true,
+          'attr-value-double-quotes': true,
+          'attr-value-not-empty': true,
+          'doctype-first': true,
+          'head-script-disabled': true,
+          'id-class-value': true,
+          'id-unique': true,
+          'img-alt-require': true,
+          'spec-char-escape': true,
+          'src-not-empty': true,
+          'style-disabled': true,
+          'tag-pair': true,
+          'tag-self-close': true,
+          'tagname-lowercase': true
+        },
+        src: ['index.html']
+      }
+    },
+    jasmine: {
+      pivotal: {
+        src: 'assets/js/**/*.js',
+        options: {
+          specs: 'spec/*Spec.js',
+          helpers: 'spec/*Helper.js'
+        }
+      }
+    },
+    uglify: {
+      build: {
+        files: {
+          'build/js/main.min.js': ['assets/js/jquery-2.0.3.min.js', 'assets/js/main.js']
+        }
+      }
+    },
+    sass: {
+      build: {
+        files: {
+          'build/css/main.css': 'assets/scss/main.scss'
+        }
+      }
+    },
+    watch: {
+      css: {
+        files: ['assets/scss/**/*.scss'],
+        tasks: ['buildcss']
+      },
+      html: {
+        files: ['index.html'],
+        tasks: ['buildhtml']
+      },
+      js: {
+        files: ['assets/js/main.js'],
+        tasks: ['buildjs']
+      },
+      specs: {
+        files: ['spec/*Spec.js'],
+        tasks: ['jasmine']
+      }
     }
-   }
   });
 
   grunt.registerTask('default', []);
   grunt.registerTask('buildcss', ['sass', 'cssc', 'cssmin']);
+  grunt.registerTask('buildhtml', ['htmlhint', 'copy:indexHtml']);
+  grunt.registerTask('buildjs', ['uglify', 'jasmine']);
 
 };
---
+++ spec/mainSpec.js
@@ -0,0 +1,15 @@
+
+$(function() {
+
+  describe("Change Player", function() {
+
+    var i = true;
+
+    it("Checks for current player and changes it", function() {
+
+        expect(i).toEqual(true);
+
+    });
+  });
+
+})
